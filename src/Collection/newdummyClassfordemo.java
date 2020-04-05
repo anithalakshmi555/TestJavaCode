c79f6411647af1bac1ae88d1bb6c6228a57283f8
anithalakshmi <manju.swamy78@gmail.com>
2020-04-05 09:49:55 +0530

    added src/Collection/newdummyClassfordemo.java

---
+++ src/Collection/newdummyClassfordemo.java
@@ -0,0 +1,12 @@
+package Collection;
+
+public class newdummyClassfordemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my dummy code ");
+
+	}
+
+}
